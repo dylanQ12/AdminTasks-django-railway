97a424f7d08d88ecf568366a89ee8a7b794425ea
dylanQ12 <136197614+dylanQ12@users.noreply.github.com>
2024-05-29 15:56:36 -0500

    update file settings.py'

--- admin_tasks/settings.py
+++ admin_tasks/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = "django-insecure-w4ybp)e)r!38#m7^gzy1l6di)2#zb-m$t%))_@4ckfstsz*s4a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ["*"]
+#ALLOWED_HOSTS = ["*"]
 
 ALLOWED_HOSTS = ["web-production-62f3.up.railway.app"]
 
