b016b2defae69a74437284510a91fe5103e31672
dylanQ12 <136197614+dylanQ12@users.noreply.github.com>
2024-05-29 16:46:00 -0500

    update code

--- admin_tasks/settings.py
+++ admin_tasks/settings.py
@@ -14,13 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-w4ybp)e)r!38#m7^gzy1l6di)2#zb-m$t%))_@4ckfstsz*s4a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-#ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*"]
 
-ALLOWED_HOSTS = ["web-production-62f3.up.railway.app"]
-
-CSRF_TRUSTED_ORIGINS = ["https://web-production-62f3.up.railway.app"]
+#ALLOWED_HOSTS = ["web-production-62f3.up.railway.app"]
+#CSRF_TRUSTED_ORIGINS = ["https://web-production-62f3.up.railway.app"]
 
 LOGIN_URL = "login"
 
