5bcc6e0f2b310cec26696e10bf2165b86d97708b
dylanQ12 <136197614+dylanQ12@users.noreply.github.com>
2024-05-28 23:04:58 -0500

    update file .env.template

--- admin_tasks/settings.py
+++ admin_tasks/settings.py
@@ -16,9 +16,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-62f3.up.railway.app']
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-62f3.up.railway.app']
 
 LOGIN_URL = 'login'
 
